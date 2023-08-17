12fda9eff5f131752172e0cb0f70ae1902acc854
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-08-17 16:55:29 +0300

    add first advert routes

--- solar-project/src/app/features/advert/advert-routing.module.ts
+++ solar-project/src/app/features/advert/advert-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdvertComponent } from './advert.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'recomended'
+  },
+  {
+    path: 'recomended',
+    title: 'Рекомендации',
+    component: AdvertComponent,
+    loadChildren: () => 
+      import('./advert-rec/advert-rec-routing.module').then(
+        (m) => m.AdvertRecRoutingModule
+      )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
