0f3d597aa730e6c1ce67d3b5733bd1ce8dcc9624
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-08-17 16:54:34 +0300

    add advert root module

---
+++ solar-project/src/app/features/advert/advert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advert',
+  templateUrl: './advert.component.html',
+  styleUrls: ['./advert.component.scss']
+})
+export class AdvertComponent {
+
+}
