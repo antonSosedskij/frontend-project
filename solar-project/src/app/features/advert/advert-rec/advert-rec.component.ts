fb1cd34d85dc773c39c34da86c6b1dabec5f61f0
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-08-18 13:39:23 +0300

    implemented data transfer from child component

--- solar-project/src/app/features/advert/advert-rec/advert-rec.component.ts
+++ solar-project/src/app/features/advert/advert-rec/advert-rec.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AdvertRecComponent {
 
+  adverts = [
+    {
+      title: 'Продам гараж',
+      price: 200000,
+      address: 'г.Нигде, ул.Никакая',
+      created_at: 'Севодня, 14,12'
+    },
+    {
+      title: 'Daewoo MatiZ',
+      price: 250000,
+      address: 'г.Нигде, ул.Никакая',
+      created_at: 'Севодня, 14:21'
+    },
+    {
+      title: 'Opel Astra, 2013',
+      price: 200000,
+      address: 'г.Нигде, ул.Никакая',
+      created_at: 'Вчера, 14:43'
+    },
+    {
+      title: 'Продам гараж',
+      price: 200000,
+      address: 'г.Нигде, ул.Никакая',
+      created_at: 'Севодня, 14,12'
+    },
+
+  ]
+
 }
