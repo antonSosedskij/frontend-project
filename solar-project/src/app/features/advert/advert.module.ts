d49ea0355d044d56a2b08c0435ac9a097fcbed93
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-10-15 11:35:07 +0300

    импорт модулей

--- solar-project/src/app/features/advert/advert.module.ts
+++ solar-project/src/app/features/advert/advert.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdvertRoutingModule } from './advert-routing.module';
-import { AdvertRecComponent } from './advert-rec/advert-rec.component';
 import { RouterModule } from '@angular/router';
 import { AdvertComponent } from './advert.component';
 import { AdvertRecModule } from './advert-rec/advert-rec.module';
-import { CardModule } from 'src/app/shared/components/card/card.module';
 import { MyAdvertsModule } from './my-adverts/my-adverts.module';
 import { OverviewModule } from './overview/overview.module';
 import { CreateModule } from './create/create.module';
+import { GalleriaModule } from 'primeng/galleria';
+import { AdvertsSearchModule } from './adverts-search/adverts-search.module';
 
 
 @NgModule({
@@ -23,7 +22,9 @@ import { CreateModule } from './create/create.module';
     AdvertRecModule,
     MyAdvertsModule,
     OverviewModule,
-    CreateModule
+    AdvertsSearchModule,
+    CreateModule,
+    GalleriaModule
   ]
 })
 export class AdvertModule { }
