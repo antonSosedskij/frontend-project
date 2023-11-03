90e47a9abefd4111ff58577b7948b9eeb6b2bf09
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-03 12:04:54 +0300

    добавил данные в environment

--- solar-project/src/environment/environment.ts
+++ solar-project/src/environment/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    apiUrl: 'http://194.87.237.48:5000/'
+    apiUrl: 'http://194.87.237.48:5000/',
+    dadataToken: 'c9583504fadb40978a9e02f47b65082cbe41fa22',
+    apiKeyYa: 'c76d01d8-b1e3-45c7-b2b2-0810f4d09651'
 };
