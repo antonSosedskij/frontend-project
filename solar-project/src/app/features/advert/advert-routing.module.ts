9e7f9327d1d0557be27724117f69aa0085b42ae6
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-11-07 01:31:03 +0300

    добавил защиту роутов

--- solar-project/src/app/features/advert/advert-routing.module.ts
+++ solar-project/src/app/features/advert/advert-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdvertComponent } from './advert.component';
+import { AuthGuard } from 'src/app/shared/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -18,6 +19,7 @@ const routes: Routes = [
       ),
   },
   {
+    canActivate: [AuthGuard],
     path: 'mine',
     title: 'Мои объявления',
     component: AdvertComponent,
@@ -36,6 +38,7 @@ const routes: Routes = [
       )
   },
   {
+    canActivate: [AuthGuard],
     path: 'create',
     title: 'Создать объявление',
     component: AdvertComponent,
